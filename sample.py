03ffac37d0315be668be01801d55ff8df7e2e559
Elango SK <elaa0505@users.noreply.github.com>
2020-01-07 17:49:22 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -7,7 +7,7 @@ import html2text
 with open("sample.html") as text_file:
     contents = text_file.read()
 #print(contents)
-openFile = open("sample.txt", "a")
+openFile = open("sample.md", "a")
 print(html2text1.html2text(contents))
 appendFile = openFile.write(html2text1.html2text(contents))
 openFile.close()
